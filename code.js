3d593ba7fb2d7ae4af033636394139ac7ad1711e
ethicnology <ethicnology@pm.me>
2022-02-17 10:27:17 +0100

    Refacto haversine formula

--- code.js
+++ code.js
@@ -34,7 +34,16 @@ document.getElementById("btn-graph").onclick = function () {
         const source = [nodes[column[0]].latitude, nodes[column[0]].longitude];
         const target = [nodes[column[1]].latitude, nodes[column[1]].longitude];
         const polyline = [source, target];
-        L.polyline(polyline, { color: "blue" }).addTo(map);
+        const distance = haversine_distance(
+          source[0],
+          source[1],
+          target[0],
+          target[1],
+        );
+        L.polyline(polyline, { color: "blue" }).addTo(map).bindTooltip(
+          `${distance.toFixed(2)} m`,
+          { sticky: true },
+        );
       }
     }
   }
@@ -59,4 +68,16 @@ function displayNodes(nodes) {
       },
     ).addTo(map).bindTooltip(node);
   }
+}
+
+function haversine_distance(latitude1, longitude1, latitude2, longitude2) {
+  const R = 6371008.7714; // IUGG  mean earth radius
+  const d_lat = (latitude2 - latitude1) * (Math.PI / 180);
+  const d_lon = (longitude2 - longitude1) * (Math.PI / 180);
+  const lat1 = latitude1 * (Math.PI / 180);
+  const lat2 = latitude2 * (Math.PI / 180);
+  const a = Math.sin(d_lat / 2) * Math.sin(d_lat / 2) +
+    Math.sin(d_lon / 2) * Math.sin(d_lon / 2) * Math.cos(lat1) * Math.cos(lat2);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  return R * c;
 }
